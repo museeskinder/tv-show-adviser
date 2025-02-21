328a7fb8dd84a574695a636fa861c143955b032e
museeskinder <eskindermuse@gmail.com>
2025-02-21 16:36:52 +0300

    project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import s from "./style.module.css";
 export function App() {
     return (
-        <div>
+        <div className={s.main_container}>
+            <div className={s.header}>
+                <div className="row">
+                    <div className="col-4">
+                        <div>LOGO</div>
+                        <div>Subtitle</div>
+                    </div>
+                    <div className="col-md-12 col-lg-4">
+                        <input style={{width: '100%'}} type="search" />
+                    </div>
+                </div>
+            </div>
+            <div className={s.tv_show_detail}></div>
+            <div className={s.recommended_tv_shows}></div>
         </div>
     );
-}
+} 
