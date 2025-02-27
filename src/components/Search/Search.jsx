52f0ec34d68d617299fab710f2abbb78b3d4297d
museeskinder <eskindermuse@gmail.com>
2025-02-27 20:41:37 +0300

    Enhance Search component to manage input state and clear on submit

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,16 +1,25 @@
 import s from "./style.module.css";
 import { Search as SearchIcon } from "react-bootstrap-icons";
+import { useState } from "react";
 
 export const Search = ({ onSubmit }) => {
+  const [value, setValue] = useState('');
+
   const submit = (e) => {
-    if(e.key === "Enter" && e.target.value.trim() !== "")
+    if(e.key === "Enter" && e.target.value.trim() !== "") {
         onSubmit(e.target.value.trim());
+        setValue("");
+    }
+  };
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
   };
 
   return (
     <>
       <SearchIcon size={27} className={s.icon} />
-      <input onKeyUp={submit} className={s.input} type="text" placeholder={"Search a tv show you may like"}/>
+      <input value={value} onChange={handleChange} onKeyUp={submit} className={s.input} type="text" placeholder={"Search a tv show you may like"}/>
     </>
   );
 };
